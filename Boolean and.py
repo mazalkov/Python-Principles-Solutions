1bbbfd8d29c6f2c48db290f8d443909c233ac846
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-04-29 08:53:43 +0200

    Create Boolean and.py

---
+++ Boolean and.py
@@ -0,0 +1,8 @@
+# https://pythonprinciples.com/challenges/Boolean-and/
+
+
+def triple_and(param1, param2, param3):
+    
+    # has to be double brackets as
+    # all can only take one argument
+    return all((param1, param2, param3))
